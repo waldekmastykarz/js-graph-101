2cd495ca91e3d1bee6a4cf7fa874d6fc34eb4e9a
waldekmastykarz <waldek@mastykarz.nl>
2022-09-16 16:30:50 +0200

    Adds setup script

--- setup.sh
+++ setup.sh
@@ -1,9 +1,18 @@
 #!/usr/bin/env bash
 
 # login
-npx -p @pnp/cli-microsoft365@next -- m365 login --authType browser
+echo "Sign in to Microsoft 365..."
+npx -p @pnp/cli-microsoft365 -- m365 login --authType browser
 
 # create AAD app
-npx -p @pnp/cli-microsoft365@next -- m365 aad app add --name "Graph JS 101" --multitenant --redirectUris "https://localhost" --platform spa
+echo "Creating AAD app..."
+appId=$(npx -p @pnp/cli-microsoft365 -- m365 aad app add --name "Graph JS 101" --multitenant --redirectUris "http://localhost/index.html,http://localhost/index_esm.html" --platform spa --query "appId" -o text)
 
 # write app to env.js
+echo "Writing app to env.js..."
+echo "const appId = '$appId';" > env.js
+
+# write app to env_esm.js
+echo "export const appId = '$appId';" > env_esm.js
+
+echo "DONE"
